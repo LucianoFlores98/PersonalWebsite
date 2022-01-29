087b7b079a3d48af2fa4c8d6c189eea39b58f22a
Luciano <lucianoaf-98@hotmail.com.ar>
2022-01-29 13:40:13 -0300

    AboutMe finished

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
-// Get the video
-var video = document.getElementById("myVideo");
 
-// Get the button
-var btn = document.getElementById("myBtn");
+// Pause and play the video, and change the icons
+function video_pause_start() {
 
-// Pause and play the video, and change the button text
-function myFunction() {
+  // Get the video
+  var video = document.getElementById("myVideo");
+
+  // Get the button
+  var btn = document.getElementById("Btn_Video");
+
+  //get the icons
+  var i_play = document.getElementById("i_play");
+  var i_pause = document.getElementById("i_pause");
+
+
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
+    i_pause.style.display = "block";
+    i_play.style.display = "none";
+
   } else {
     video.pause();
-    btn.innerHTML = "Play";
+    i_pause.style.display = "none";
+    i_play.style.display = "block";
+
   }
+}
+
+//---------------------------------------------------------------
+
+function cv_show_hide() {
+
+  var cv = document.getElementById("CVContainer");
+  var button = document.getElementById("CVButton-close");
+
+
+  if (cv.style.display === "none") {
+    cv.style.display = "flex";
+    button.style.display = "block";
+
+  } else {
+    cv.style.display = "none";
+    button.style.display = "none";
+  }
+  
 }
